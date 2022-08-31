4493f9e9b6ba3f8f009233030b37b0905a66b934
Laurehc <111622067+Laurehc@users.noreply.github.com>
2022-08-31 02:38:40 -0500

    Update and rename Capítulo3_ejercicio21.java to Capitulo3_ejercicio21.java

--- Actividad 2/Capitulo3_ejercicio21.java
+++ Actividad 2/Capitulo3_ejercicio21.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EjerciciosPoo { 
+public class Capitulo3_ejercicio21 { 
     public static void main(String[] args) {
         /* Dados los tres lados de un triángulo, haga un algoritmo que encuentre: perímetro,
 semiperímetro y el área del triángulo. */
