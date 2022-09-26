2e9a9c4ee865ca1bdf9c182cb10c8cfc270666e9
brokie636 <104879191+brokie636@users.noreply.github.com>
2022-09-26 17:17:20 -0500

    Add files via upload

---
+++ Actividad 3/Triangulo/Triangulo/src/main/java/com/mycompany/triangulo/Calculos.java
@@ -0,0 +1,15 @@
+package com.mycompany.triangulo;
+
+public class Calculos {
+    
+    
+    public static double calcularPerimetro(double lado){
+        return lado*4;
+    }
+    public static double calcularAltura(double lado){
+        return ((Math.sqrt(3)*lado)/2);
+    }
+    public static double calcularArea(double lado, double calcularAltura){
+        return lado*((Math.sqrt(3)*lado)/2)/2;
+    }
+}
---
+++ Actividad 3/Triangulo/Triangulo/src/main/java/com/mycompany/triangulo/Triangulo.java
@@ -0,0 +1,12 @@
+
+
+package com.mycompany.triangulo;
+
+
+public class Triangulo {
+
+    public static void main(String[] args) {
+       CalcularTriangulo formulario = new CalcularTriangulo();
+       formulario.setVisible(true);
+    }
+}
