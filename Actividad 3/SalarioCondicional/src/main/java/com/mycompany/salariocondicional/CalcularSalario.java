116a1e245d2ae33dbfcc1cc56bdfffdd7b8320cd
brokie636 <104879191+brokie636@users.noreply.github.com>
2022-09-26 17:15:31 -0500

    Add files via upload

---
+++ Actividad 3/SalarioCondicional/src/main/java/com/mycompany/salariocondicional/CalcularSalario.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.salariocondicional;
+
+public class CalcularSalario {
+    public static double Salario(double SalarioHora, double horas){
+        return SalarioHora*horas;
+}
+}
---
+++ Actividad 3/SalarioCondicional/src/main/java/com/mycompany/salariocondicional/SalarioCondicional.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.salariocondicional;
+
+public class SalarioCondicional {
+
+    public static void main(String[] args) {
+        FormularioEmpleado formulario = new FormularioEmpleado();
+        formulario.setVisible(true);
+    }
+}
