dd39390ce3396214956b37e1fd227d1d4abf6092
brokie636 <104879191+brokie636@users.noreply.github.com>
2022-09-26 17:09:22 -0500

    Add files via upload

---
+++ Actividad 3/Potencias/src/main/java/com/mycompany/potencias/CalcularPotencias.java
@@ -0,0 +1,14 @@
+package com.mycompany.potencias;
+
+public class CalcularPotencias {
+   public static Double Cuadrado(double A){
+       return Math.pow(A,2);
+   } 
+   public static Double Cubo(double A){
+       return Math.pow(A,3);
+   }
+   public static Double Sqrt(double A){
+       return Math.sqrt(A);
+   }
+
+}
