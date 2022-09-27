7d3839557d96b6e27036d603042b6ff4c934a3c1
brokie636 <104879191+brokie636@users.noreply.github.com>
2022-09-26 19:17:20 -0500

    Update Calculos.java

--- Actividad 3/Triangulo/Triangulo/src/main/java/com/mycompany/triangulo/Calculos.java
+++ Actividad 3/Triangulo/Triangulo/src/main/java/com/mycompany/triangulo/Calculos.java
@@ -4,7 +4,7 @@ public class Calculos {
     
     
     public static double calcularPerimetro(double lado){
-        return lado*4;
+        return lado*3;
     }
     public static double calcularAltura(double lado){
         return ((Math.sqrt(3)*lado)/2);
