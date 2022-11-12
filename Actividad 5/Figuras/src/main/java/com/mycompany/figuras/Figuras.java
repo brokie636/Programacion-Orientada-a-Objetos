d8f72ca5d99fd757522836c48bd63815dd168336
brokie636 <104879191+brokie636@users.noreply.github.com>
2022-11-11 19:29:05 -0500

    Add files via upload

---
+++ Actividad 5/Figuras/src/main/java/com/mycompany/figuras/Figuras.java
@@ -0,0 +1,13 @@
+package com.mycompany.figuras;
+
+public class Figuras {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; 
+        miVentanaPrincipal= new VentanaPrincipal(); 
+        miVentanaPrincipal.setVisible(true); 
+        miVentanaPrincipal.setResizable(false);
+
+
+    }
+}
