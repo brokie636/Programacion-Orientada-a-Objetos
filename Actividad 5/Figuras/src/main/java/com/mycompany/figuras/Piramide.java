cda1c36a15641e119d26fb03b498fa497bfea62c
brokie636 <104879191+brokie636@users.noreply.github.com>
2022-11-11 19:16:45 -0500

    Add files via upload

---
+++ Actividad 5/Figuras/src/main/java/com/mycompany/figuras/Piramide.java
@@ -0,0 +1,25 @@
+package com.mycompany.figuras;
+
+public class Piramide extends FiguraGeometrica{
+    private double base;
+    private double altura;
+    private double apotema;
+    
+    public Piramide(double base, double altura, double apotema){
+        this.base=base;
+        this.altura=altura;
+        this.apotema=apotema;
+        this.setVolumen(calcularVolumen());
+        this.setSuperficie(calcularSuperficie());
+    }
+    
+    public double calcularVolumen(){
+        double volumen=(Math.pow(base,2)*altura)/3;
+        return volumen;
+    }
+    public double calcularSuperficie(){
+        double areaBase=Math.pow(base,2);
+        double areaLado=2*base*apotema;
+        return areaBase+areaLado;
+    }
+}
