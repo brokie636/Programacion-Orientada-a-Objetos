d983cba2ea162d2a81a930ed562a958f79cb3b5d
AlejandroTorradoCalderon <112442070+AlejandroTorradoCalderon@users.noreply.github.com>
2022-11-19 16:39:46 -0500

    Add files via upload

---
+++ Actividad 5/sistema de informacion/Corriente.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.universidadnacional;
+
+public class Corriente {
+    public static double calcular_corriente(double voltaje, double resistencia){
+        double corriente;
+        corriente = voltaje / resistencia;
+        return corriente;
+}}
---
+++ Actividad 5/sistema de informacion/UniversidadNacional.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.universidadnacional;
+import javax.swing.JFrame;
+
+public class UniversidadNacional {
+
+    public static void main(String[] args) {
+        frmPrincipal principal = new frmPrincipal();
+        principal.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        principal.setVisible(true);
+    }
+}
